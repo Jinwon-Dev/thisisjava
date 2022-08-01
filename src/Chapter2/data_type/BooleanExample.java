a37ce54490f8eb66ea02b5330db989107abd7669
Jinwon-Dev <yjw9424@naver.com>
2022-08-02 02:47:33 +0900

    데이터 타입

---
+++ src/Chapter2/data_type/BooleanExample.java
@@ -0,0 +1,13 @@
+/* boolean 타입 */
+package Chapter2.data_type;
+
+public class BooleanExample {
+    public static void main(String[] args) {
+        boolean stop = true;
+        if (stop) {
+            System.out.println("중지합니다.");
+        } else {
+            System.out.println("시작합니다");
+        }
+    }
+}
