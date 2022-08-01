4c0d35a107b8329c5a0c1cf10bae560ec631078d
Jinwon-Dev <yjw9424@naver.com>
2022-08-02 02:45:45 +0900

    자바 기본 실습

---
+++ src/Chapter1/Hello.java
@@ -0,0 +1,14 @@
+/* 자바 소스파일, 주석 사용하기 */
+package Chapter1;
+
+/*
+작성자 : 윤진원
+작성일 : 2022.8.1
+ */
+public class Hello {
+    //프로그램 실행 진입점
+    public static void main(String[] args) {
+        //콘솔에 출력하는 실행문
+        System.out.println("Hello, welcome to the java world!");
+    } //end of main
+} //end of class
