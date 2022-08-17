cd7f443245da3331c3be0d63df201f7f4cede9f7
Jinwon-Dev <yjw9424@naver.com>
2022-08-18 07:30:31 +0900

    필드

---
+++ src/Chapter6/field/Car.java
@@ -0,0 +1,11 @@
+/* Car 클래스 필드 선언 */
+package Chapter6.field;
+
+public class Car {
+    // 필드
+    String company = "현대자동차";
+    String model = "그랜저";
+    String color = "검정";
+    int maxSpeed = 350;
+    int speed;
+}
---
+++ src/Chapter6/field/FieldInitValue.java
@@ -0,0 +1,19 @@
+/* 필드 자동 초기화 */
+package Chapter6.field;
+
+public class FieldInitValue {
+    // 필드
+    byte byteField;
+    short shortField;
+    int intField;
+    long longField;
+
+    boolean booleanField;
+    char charField;
+
+    float floatField;
+    double doubleField;
+
+    int[] arrField;
+    String referenceField;
+}
