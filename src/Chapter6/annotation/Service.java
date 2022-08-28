144dc32a59faa7a915aa40aa789312cbf8ac40cb
Jinwon-Dev <yjw9424@naver.com>
2022-08-28 20:17:01 +0900

    어노테이션

---
+++ src/Chapter6/annotation/Service.java
@@ -0,0 +1,19 @@
+/* 어노테이션 적용 클래스 */
+package Chapter6.annotation;
+
+public class Service {
+    @PrintAnnotation
+    public void method1() {
+        System.out.println("실행 내용1");
+    }
+
+    @PrintAnnotation("*")
+    public void method2() {
+        System.out.println("실행 내용2");
+    }
+
+    @PrintAnnotation(value = "#", number = 20)
+    public void method3() {
+        System.out.println("실행 내용3");
+    }
+}
