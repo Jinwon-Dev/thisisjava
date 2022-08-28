f8a687acf1f4a082d7f8b67e7d70d40dc24c4f87
Jinwon-Dev <yjw9424@naver.com>
2022-08-28 20:11:54 +0900

    생성자

---
+++ src/Chapter6/constructor/constructor_declaration/Car.java
@@ -0,0 +1,9 @@
+/* 생성자 선언 */
+package Chapter6.constructor.constructor_declaration;
+
+public class Car {
+    // 생성자
+    Car(String color, int cc) {
+
+    }
+}
---
+++ src/Chapter6/constructor/constructor_declaration/CarExample.java
@@ -0,0 +1,9 @@
+/* 생성자를 호출해서 객체 생성 */
+package Chapter6.constructor.constructor_declaration;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car("검정", 3000);
+        //Car myCar = new Car(); (x)
+    }
+}
