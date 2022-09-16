bbdbf7047723cea7d3d07b5cbf342b72dc8858f6
Jinwon-Dev <yjw9424@naver.com>
2022-09-16 16:07:09 +0900

    Chapter 7 exercise

---
+++ src/java_exercise/Chapter7/SnowTire.java
@@ -0,0 +1,8 @@
+package java_exercise.Chapter7;
+
+public class SnowTire extends Tire {
+    @Override
+    public void run() {
+        System.out.println("스노우 타이어가 굴러갑니다.");
+    }
+}
---
+++ src/java_exercise/Chapter7/SnowTireExample.java
@@ -0,0 +1,11 @@
+package java_exercise.Chapter7;
+
+public class SnowTireExample {
+    public static void main(String[] args) {
+        SnowTire snowTire = new SnowTire();
+        Tire tire = snowTire;
+
+        snowTire.run();
+        tire.run();
+    }
+}
