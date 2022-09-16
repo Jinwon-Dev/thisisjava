d937b53baeda9288fa57b41be59cc246d2d3793d
Jinwon-Dev <yjw9424@naver.com>
2022-09-16 16:02:26 +0900

    부모 생성자 호출

---
+++ src/Chapter7/parent_constructor_call/People.java
@@ -0,0 +1,12 @@
+/* 부모 클래스 */
+package Chapter7.parent_constructor_call;
+
+public class People {
+    public String name;
+    public String ssn;
+
+    public People(String name, String ssn) {
+        this.name = name;
+        this.ssn = ssn;
+    }
+}
---
+++ src/Chapter7/parent_constructor_call/StudentExample.java
@@ -0,0 +1,11 @@
+/* 자식 객체 이용 */
+package Chapter7.parent_constructor_call;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        Student student = new Student("홍길동", "123456-1234567", 1);
+        System.out.println("name : " + student.name);
+        System.out.println("ssn : " + student.ssn);
+        System.out.println("studentNo : " + student.studentNo);
+    }
+}
