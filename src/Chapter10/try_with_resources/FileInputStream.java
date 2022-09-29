78529d476d9eeef0b05cb7cedda03784ec68120f
Jinwon-Dev <yjw9424@naver.com>
2022-09-29 15:54:08 +0900

    자동 리소스 닫기

---
+++ src/Chapter10/try_with_resources/FileInputStream.java
@@ -0,0 +1,19 @@
+/* AutoCloseable 구현 클래스 */
+package Chapter10.try_with_resources;
+
+public class FileInputStream implements AutoCloseable {
+    private String file;
+
+    public FileInputStream(String file) {
+        this.file = file;
+    }
+
+    public void read() {
+        System.out.println(file + "을 읽습니다.");
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println(file + "을 닫습니다.");
+    }
+}
