c5d38703b42d371ce96f8ecd13c2cc1f8dd87819
Jinwon-Dev <yjw9424@naver.com>
2022-11-11 12:35:05 +0900

    List 컬렉션

---
+++ src/Chapter15/list_collection/vector/Board.java
@@ -0,0 +1,14 @@
+/* 게시물 정보 객체 */
+package Chapter15.list_collection.vector;
+
+public class Board {
+    String subject;
+    String content;
+    String writer;
+
+    public Board(String subject, String content, String writer) {
+        this.subject = subject;
+        this.content = content;
+        this.writer = writer;
+    }
+}
